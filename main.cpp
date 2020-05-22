d49217f03286810414c80df148436dc5883b5e90
CloudStrifeUA <vladko1406@gmail.com>
2020-05-22 18:40:43 +0300

    Fixed computational problems

--- main.cpp
+++ main.cpp
@@ -1,64 +1,72 @@
 #include <iostream>
-#include "node.h"
 #include <random>
 #include <chrono>
 #include <iomanip>
+#include <fstream>
 
-double factor(int n)
+#define LAMBDA 1.0
+#define NU 10
+#define RHO LAMBDA/NU
+#define MIN_REPLACEMENT 3
+#define MAX_REPLACEMENT 100
+#define T 10
+#define N 1e+04
+struct Logger
+{
+    std::ostream& stream;
+    Logger(std::ostream& os):stream(os){}
+};
+
+long double factor(int n)
 {
     if(n == 1 || n == 0)
         return 1;
     return n*factor(n - 1);
 }
 
-double poisson_prob(double lambda, int k)
+long double poisson_prob(long double lambda, int k)
 {
-    return std::pow(lambda, k)* std::exp(-lambda)/factor(k);
+    return powl(lambda, k)* expl(-lambda)/factor(k);
 }
 
-double poisson_sum(double lambda, int u)
+long double poisson_sum(long double lambda, int u)
 {
-    double res = 0;
-    for(int i = 0; i < u; i++)
+    long double res = 0;
+    for(int i = u; i < u + 10; i++)
     {
         res += poisson_prob(lambda, i);
     }
     return res;
 }
 
+long double q(int n)
+{
+    return (1 - RHO)*powl(RHO, n - 1)/(1 - powl(RHO, n));
+}
 
-int main() {
-    //region Генератори
+long double potf(const double lambda, const double nu, const double t, const uint replacement, const uint n, std::vector<double> &probs)
+{
+//region Генератори
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::knuth_b g(seed);
-    const double lambda = 1, nu = 1;
-    auto *r = new std::exponential_distribution<double>(lambda);    //
     auto *r1 = new std::exponential_distribution<double>(nu);      //
-    // auto *r2 = new std::exponential_distribution<double>(eta);      //
-    //endregion
-    const unsigned replacement = 1;
-    const double t = 1;
-    const int n = 1, m = 1, N = 1e+03;
-    double res = 0;
-    for(int i = 0; i < N; i++)
+//endregion
+    long double res = 0;
+    for(int i = 0; i < n; i++)
     {
-        double time = 0.0, p = 1.0;
+        long double time = 0.0, p = 1.0;
         int q = 0;
         while(time < t)
         {
-            double dt = t - time;       //t - t_k
-            double eta = (*r1)(g);
-            std::cout << "dt " << dt << std::endl;
-            std::cout << "eta " << eta << std::endl;
+            long double dt = t - time;       //t - t_k
+            long double eta = (*r1)(g);
             if(q == 0)
             {
-                p *= (1 - std::exp(-lambda * dt));
-                double tau = -std::log(1 - (std::rand()/(double)RAND_MAX) * (1 - std::exp(-lambda * dt)))/lambda;
-                std::cout << "tau " << tau << std::endl;
+                p *= (1 - expl(-lambda * dt));
+                long double tau = -logl(1 - (std::rand()/(long double)RAND_MAX) * (1 - expl(-lambda * dt)))/lambda;
                 if(tau + eta >= dt)
                 {
-                    p *= 1 - poisson_sum(lambda*(dt - tau), replacement);
-                    std::cout << "p " << p << std::endl;
+                    p *= poisson_sum(lambda*(dt - tau), replacement);
                     break;
                 }
                 else
@@ -66,10 +74,9 @@ int main() {
                     auto *m = new std::poisson_distribution<int>(lambda*eta);
                     int m_k = (*m)(g);
                     delete m;
-                    std::cout << "m_k " << m_k << std::endl;
                     if(m_k >= replacement)
                         break;
-                    else q+= m_k;
+                    else q += m_k;
                     time += eta + tau;
                 }
             }
@@ -77,8 +84,7 @@ int main() {
             {
                 if(eta >= dt)
                 {
-                    p *= 1 - poisson_sum(lambda*dt, replacement + 1 - q);
-                    std::cout << "p " << p << std::endl;
+                    p *= poisson_sum(lambda*dt, replacement + 1 - q);
                     break;
                 }
                 else
@@ -86,7 +92,6 @@ int main() {
                     auto *m = new std::poisson_distribution<int>(lambda*eta);
                     int m_k = (*m)(g);
                     delete m;
-                    std::cout << "m_k " << m_k << std::endl;
                     if(m_k >= replacement + 1 - q)
                         break;
                     else q+= m_k - 1;
@@ -95,8 +100,35 @@ int main() {
                 
             }
         }
+        probs.push_back(p);
         res += p;
     }
-    std::cout << 1 - res/N << std::endl;
+    return res/n;
+}
+
+int main() 
+{
+    std::ofstream f("log.txt", std::ios_base::out|std::ios_base::app);
+    Logger logger(f);
+    logger.stream << "lambda = " << LAMBDA << ", nu = " << NU << ", t = " << T << ", N = " << N << std::endl;
+    logger.stream << std::setw(5) << "n" << " |" << std::setw(15) << "delta(t,n) " << std::setw(6) << "|" << " delta(t,n)/(rho^n) " << "|"
+    <<  "delta(t,n)/(n*rho^n)" << "|" << " Iмовірність відмови" << "|" << std::setw(11) << "s^2" << std::setw(10) 
+    << "|" << std::endl;
+    long double a = 1/(LAMBDA*(1 - RHO));
+    std::vector<double> probs;
+    for(size_t u = MIN_REPLACEMENT; u <= MAX_REPLACEMENT; u++)
+    {
+        long double p = potf(LAMBDA, NU, T, u, N, probs);
+        long double div = 0.0;
+        for(auto i:probs)
+        {
+            div += (i - p)*(i - p);
+        }
+        div /= probs.size() - 1;
+        long double flQ = q(u);
+        long double delta = fabsl(p - flQ > 0.01 ? (1 - expl(-T*q(u)/a)):(T*flQ/a - powl(T*flQ/a, 2)/2 + powl(T*flQ/a, 3)/6 - powl(T*flQ/a, 4)/24));
+        logger.stream << std::setprecision(12) << std::scientific;
+        logger.stream << std::setw(5) << u << " | " << delta << " | " << delta/(powl(RHO, u))  << " | " << delta/(powl(RHO, u)*u)  << " | " << p  << " | " << div  << " | " << std::endl;
+    }
     return 0;
 }
